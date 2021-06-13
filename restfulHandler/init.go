8394db25bfa3f6ad7040eec2064bd06d46b3f59c
sicko7947 <cai139193541@gmail.com>
2021-06-14 01:23:29 +1000

    minor change

--- restfulHandler/init.go
+++ restfulHandler/init.go
@@ -1,24 +1,34 @@
 package restfulhandler
 
 import (
+	"fmt"
+	"os/exec"
+
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 )
 
+func PortInUse(port int) bool {
+	checkStatement := fmt.Sprintf("lsof -i:%d ", port)
+	output, _ := exec.Command("sh", "-c", checkStatement).CombinedOutput()
+	return len(output) > 0
+}
+
 func init() {
-	s := g.Server()
-	s.Group("/", func(group *ghttp.RouterGroup) {
-		group.Middleware(MiddlewareCORS)
+	if !PortInUse(28888) {
+		s := g.Server()
 
-		group.POST("/auth/activate", activate)
-		group.POST("/auth/reset", reset)
-	})
-	s.EnableHTTPS(`/root/auth/auth.sickoaio.com.crt`, `/root/auth/auth.sickoaio.com.key`)
-	s.SetPort(28888)
-	s.Run()
+		s.Group("/", func(group *ghttp.RouterGroup) {
+			group.POST("/auth/activate", activate)
+			group.POST("/auth/reset", reset)
+		})
+		s.EnableHTTPS(`/root/auth/auth.sickoaio.com.crt`, `/root/auth/auth.sickoaio.com.key`)
+		s.SetHTTPSPort(28888)
+		s.Run()
+	}
 }
 
-func MiddlewareCORS(r *ghttp.Request) {
-	r.Response.CORSDefault()
-	r.Middleware.Next()
-}
+// func MiddlewareCORS(r *ghttp.Request) {
+// 	r.Response.CORSDefault()
+// 	r.Middleware.Next()
+// }
